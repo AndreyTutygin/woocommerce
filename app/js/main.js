2b21eb627436ca8af1e9475c5b31f0781704d1eb
andreytutygin <tutygin.a.v@mail.ru>
2021-01-10 00:19:00 +0300

    Installed swiper.js, made a reviews section and a mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 import './components/burger';
 import './components/scroll';
 
+import Swiper from 'swiper/bundle';
+
+const swiper = new Swiper(".reviews-slider", {
+    // direction: 'vertical',
+    spaceBetween: 50,
+    speed: 800,
+    navigation: {
+        nextEl: '.arrow-btn__next',
+        prevEl: '.arrow-btn__prev',
+    },
+});
+
